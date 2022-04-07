86c868501bf27a3b6554b5dff6a4c5a338520390
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-07 13:18:26 +0430

    root execute implemented

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -1,5 +1,34 @@
 package cmd
 
-func Execute() {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
 
+	"github.com/amirhnajafiz/go-graphql/internal/gql"
+	"github.com/graphql-go/graphql"
+)
+
+func Execute() {
+	s := gql.Init()
+	query := `
+		{
+			authors {
+				Name
+				Books {
+					Title
+					PublishDate
+				}
+			}
+		}
+	`
+
+	params := graphql.Params{Schema: s, RequestString: query}
+	r := graphql.Do(params)
+	if len(r.Errors) > 0 {
+		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
+	}
+
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON)
 }
