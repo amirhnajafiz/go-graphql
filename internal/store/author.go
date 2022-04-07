40f93eb27956cf53127d8052d7030b39b951ccef
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-07 12:58:51 +0430

    store struct types added

--- internal/store/author.go
+++ internal/store/author.go
@@ -1,1 +1,6 @@
 package store
+
+type Author struct {
+	Name  string
+	Books []Book
+}
--- internal/store/book.go
+++ internal/store/book.go
@@ -1,1 +1,11 @@
 package store
+
+import "time"
+
+type Book struct {
+	Title       string
+	Reference   string
+	PublishDate time.Time
+	Sells       int
+	Price       float64
+}
