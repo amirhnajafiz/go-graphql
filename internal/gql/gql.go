4f1d0b4df118de738b796b9c4f329a9c427758aa
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-07 13:16:32 +0430

    gql init implemented

--- internal/gql/gql.go
+++ internal/gql/gql.go
@@ -1,5 +1,24 @@
 package gql
 
-func Init() {
+import (
+	"log"
 
+	"github.com/graphql-go/graphql"
+)
+
+func Init() graphql.Schema {
+	fields := graphql.Fields{
+		"Authors": &graphql.Field{
+			Type: AuthorType(),
+		},
+	}
+
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
+
+	return schema
 }
