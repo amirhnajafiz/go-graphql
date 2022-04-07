4f178aef86a630da413905edd397c147ff66f21f
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-07 13:12:28 +0430

    book type added for graphql

--- internal/gql/types.go
+++ internal/gql/types.go
@@ -6,13 +6,36 @@ import (
 
 func AuthorType() *graphql.Object {
 	return graphql.NewObject(graphql.ObjectConfig{
-		Name: "author",
+		Name: "Author",
 		Fields: graphql.Fields{
 			"Name": &graphql.Field{
 				Type: graphql.String,
 			},
 			"Books": &graphql.Field{
-				Type: graphql.NewList(graphql.Int),
+				Type: graphql.NewList(BookType()),
+			},
+		},
+	})
+}
+
+func BookType() *graphql.Object {
+	return graphql.NewObject(graphql.ObjectConfig{
+		Name: "Book",
+		Fields: graphql.Fields{
+			"Title": &graphql.Field{
+				Type: graphql.String,
+			},
+			"Reference": &graphql.Field{
+				Type: graphql.String,
+			},
+			"PublishDate": &graphql.Field{
+				Type: graphql.DateTime,
+			},
+			"Sells": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"Price": &graphql.Field{
+				Type: graphql.Float,
 			},
 		},
 	})
