e6c784354bf27afef61a1a2bab9c2eb82a66af18
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-15 12:43:06 +0430

    gql refactor

--- internal/gql/gql.go
+++ internal/gql/gql.go
@@ -1,9 +1,10 @@
 package gql
 
 import (
-	"github.com/amirhnajafiz/go-graphql/internal/store"
+	"fmt"
 	"log"
 
+	"github.com/amirhnajafiz/go-graphql/internal/store"
 	"github.com/graphql-go/graphql"
 )
 
@@ -17,12 +18,27 @@ func Init() graphql.Schema {
 			"create": store.SetupAuthorMutations(),
 		},
 	})
+
 	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: aggregateSchema}
 	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery), Mutation: aggregateMutation}
+
 	schema, err := graphql.NewSchema(schemaConfig)
 	if err != nil {
 		log.Fatalf("failed to create new schema, error: %v", err)
 	}
 
 	return schema
+}
+
+func ExecuteQuery(query string, schema graphql.Schema) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+
+	if len(result.Errors) > 0 {
+		fmt.Printf("wrong result, unexpected errors: %v", result.Errors)
+	}
+
+	return result
 }
