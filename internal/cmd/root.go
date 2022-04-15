b36b3b5ac3d07888ef3462d3c3c4f05d639347c3
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-15 13:11:56 +0430

    gin server implemented

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -1,33 +1,16 @@
 package cmd
 
 import (
-	"encoding/json"
-	"fmt"
-
+	"github.com/amirhnajafiz/go-graphql/internal/cmd/server"
 	"github.com/amirhnajafiz/go-graphql/internal/gql"
 )
 
 func Execute() {
 	s := gql.Init()
-	query := `
-		{
-			author {
-				Name: "Amir" {
-				}
-			}
-		}
-	`
-	insert := `
-		mutation {
-			create(name: "Amir") {
-				Name
-			}
-		}
-	`
+	app := server.Init(s)
 
-	_ = gql.ExecuteQuery(insert, s)
-	r := gql.ExecuteQuery(query, s)
-	rJSON, _ := json.Marshal(r)
-
-	fmt.Printf("%s \n", rJSON)
+	err := app.Run(":5000")
+	if err != nil {
+		panic(err)
+	}
 }
--- internal/cmd/server/main.go
+++ internal/cmd/server/main.go
@@ -1,1 +1,40 @@
 package server
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/amirhnajafiz/go-graphql/internal/gql"
+	"github.com/gin-gonic/gin"
+	"github.com/graphql-go/graphql"
+)
+
+func Init(s graphql.Schema) *gin.Engine {
+	app := gin.Default()
+
+	app.POST("/query", func(context *gin.Context) {
+		jsonData, err := ioutil.ReadAll(context.Request.Body)
+		if err != nil {
+			_ = context.Error(err)
+			return
+		}
+
+		var query struct {
+			Query string `json:"query"`
+		}
+
+		err = json.Unmarshal(jsonData, &query)
+		if err != nil {
+			_ = context.Error(err)
+			return
+		}
+
+		r := gql.ExecuteQuery(query.Query, s)
+		rJSON, _ := json.Marshal(r)
+
+		context.JSON(http.StatusOK, rJSON)
+	})
+
+	return app
+}
