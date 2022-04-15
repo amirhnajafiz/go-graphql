590c66366b60e1aead1ac9d14ec2f29de02f2520
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-15 12:24:17 +0430

    book type init in store

--- internal/store/book.go
+++ internal/store/book.go
@@ -1,6 +1,9 @@
 package store
 
-import "time"
+import (
+	"github.com/graphql-go/graphql"
+	"time"
+)
 
 type Book struct {
 	Title       string
@@ -8,4 +11,29 @@ type Book struct {
 	PublishDate time.Time
 	Sells       int
 	Price       float64
+}
+
+var bookType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Book",
+	Fields: graphql.Fields{
+		"Title": &graphql.Field{
+			Type: graphql.String,
+		},
+		"Reference": &graphql.Field{
+			Type: graphql.String,
+		},
+		"PublishDate": &graphql.Field{
+			Type: graphql.DateTime,
+		},
+		"Sells": &graphql.Field{
+			Type: graphql.Int,
+		},
+		"Price": &graphql.Field{
+			Type: graphql.Float,
+		},
+	},
+})
+
+func GetBookType() *graphql.Object {
+	return bookType
 }
