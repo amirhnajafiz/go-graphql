c878b88b837b0b6e11d9083aefcf2c798d0265fc
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-15 12:14:37 +0430

    author type init in store

--- internal/store/author.go
+++ internal/store/author.go
@@ -1,6 +1,22 @@
 package store
 
+import "github.com/graphql-go/graphql"
+
 type Author struct {
 	Name  string
 	Books []Book
 }
+
+var authorType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Author",
+		Fields: graphql.Fields{
+			"Name": &graphql.Field{
+				Type: graphql.String,
+			},
+			"Books": &graphql.Field{
+				Type: graphql.NewList(graphql.Int),
+			},
+		},
+	},
+)
