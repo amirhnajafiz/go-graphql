c9d8eaf1a1e68fce8c5b771fe0c4a1f8ec9a45de
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-16 11:26:29 +0430

    config package implemented

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,46 @@
 package config
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/structs"
+)
+
+type (
+	Config struct {
+	}
+)
+
+func Load() Config {
+	var instance Config
+
+	k := koanf.New(".")
+
+	// load default configuration from default function
+	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
+		log.Fatalf("error loading default: %s", err)
+	}
+
+	// load configuration from file
+	if err := k.Load(file.Provider("config.yml"), yaml.Parser()); err != nil {
+		log.Printf("error loading config.yml: %s", err)
+	}
+
+	if err := k.Unmarshal("", &instance); err != nil {
+		log.Fatalf("error unmarshalling config: %s", err)
+	}
+
+	indent, _ := json.MarshalIndent(instance, "", "\t")
+	tmpl := `
+	================ Loaded Configuration ================
+	%s
+	======================================================
+	`
+	log.Printf(tmpl, string(indent))
+
+	return instance
+}
